8cd960b10cda0c26b045899bba50c17a9a7a0385
fmot <genzzabro09812@gmail.com>
2025-04-02 14:58:56 -0700

    Add contentlayer2 setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { allPosts, Post } from "contentlayer/generated";
+import Link from "next/link";
+
+function PostCard(post: Post) {
+  return (
+    <div className="mb-8">
+      <h2 className="mb-1 text-xl">
+        <Link href={post.url}>{post.title}</Link>
+      </h2>
+    </div>
+  );
+}
+
 export default function Home() {
-  return <div></div>;
+  const posts = allPosts;
+
+  return (
+    <div className="max-w-xl mx-auto my-8">
+      <h1 className="text-center">Next.js + Contentlayer Example</h1>
+      {posts.map((post) => (
+        <PostCard {...post} key={post._id} />
+      ))}
+    </div>
+  );
 }
---
+++ contentlayer.config.ts
@@ -0,0 +1,20 @@
+import { defineDocumentType, makeSource } from "contentlayer2/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `**/*.mdx`,
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string" },
+    date: { type: "date", required: true },
+    summary: { type: "string", required: true },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({ contentDirPath: "posts", documentTypes: [Post] });
